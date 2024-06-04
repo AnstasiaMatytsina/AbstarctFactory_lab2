c67a653cb8b56abdb0fd43df9f52549d3cd578df
Anastasia Matytsina <matytsina.nastenka@mail.ru>
2024-06-04 22:32:19 +0700

    Исходный предложенный код

--- myclass.cpp
+++ myclass.cpp
@@ -1,6 +1,16 @@
 #include "myclass.h"
 
-MyClass::MyClass()
-{
-
+class MyClass {
+public:
+void testFunc1() {
 }
+virtual void testFunc3() const {
+}
+protected:
+static void testFunc4() {
+printf("Hello, world!\n");
+}
+private:
+static void testFunc2() {
+}
+};
