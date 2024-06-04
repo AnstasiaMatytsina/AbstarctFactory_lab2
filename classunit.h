9f8edd48121bfce54436f49cf568e8a5aa29d425
Anastasia Matytsina <matytsina.nastenka@mail.ru>
2024-06-04 22:31:50 +0700

    Предложенный код
    Размещение исходного предложенного кода

---
+++ classunit.h
@@ -0,0 +1,11 @@
+#ifndef CLASSUNIT_H
+#define CLASSUNIT_H
+
+
+class ClassUnit
+{
+public:
+    ClassUnit();
+};
+
+#endif // CLASSUNIT_H
---
+++ printoperatorunit.h
@@ -0,0 +1,11 @@
+#ifndef PRINTOPERATORUNIT_H
+#define PRINTOPERATORUNIT_H
+
+
+class PrintOperatorUnit
+{
+public:
+    PrintOperatorUnit();
+};
+
+#endif // PRINTOPERATORUNIT_H
